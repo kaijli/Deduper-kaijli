f648cea7857af2dc7a4fbac1b5f2dd3847a92140
kaijli <kaitlyn.j.li@gmail.com>
2022-10-18 16:00:49 -0700

    working on soft clipping

--- deduper.py
+++ deduper.py
@@ -20,31 +20,80 @@ output_name = args.outputfile
 umi_name = args.umi
 
 # test files
-file_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/sorted_test.sam"
+input_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/sorted_test.sam"
 umi_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/STL96.txt"
+output_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/test_out.sam"
 
-def read_umi_file(filename: str) -> set:
+def check_strand(flag: int) -> bool:
     '''
-    read text file containing known UMIs in the reads
-    ''' 
-    umi_set = set()
-    with open(filename, "r") as fh:
-        for line in fh:
-            line = line.strip()
-            umi_set.add(line)
-    return umi_set
+    Takes a flag integer and determines whether 16th bit is flipped.
+    If 16th bit is flipped (1), then it's the reverse strand.
+    If 16th bit is not flipped (0), then it's the forward strand. 
+    '''
+    if (flag & 16) != 16:   # check if 16th bit is flipped
+        fwd = True          # 16th bit is not flipped, 0
+    else:
+        fwd = False         # 16th bit is flipped, 1
+    return fwd
 
-with open(input_name, "r") as fh:
-    header = list()
-    collection = dict()
-    # qname,flag,rname,pos,mapq,cigar,rnext,pnext,tlen,seq,qual = "","","","","","","","","","",""
+def soft_clip(cigar: str, fwd: bool, pos: int) -> int:
+    '''
+    Takes CIGAR string, which strand the read is on, and the left-most starting position.
+    Looks for front and end softclipping in CIGAR string using 'S'.
+    Determines which clipping value to subtract from starting position based on strand.
+    '''
+    front_clip = 0
+    end_clip = 0
+    f = re.search(r'(^\d+)S', cigar)
+    e = re.search(r'(\d+)S$', cigar)
+    if isinstance(f.group(0), int):
+        front_clip = f.group(0)
+
+    if isinstance(e.group(0), int):
+        end_clip = e.group(0)
+    
+    if fwd:
+        pos = pos - int(front_clip)
+    elif not fwd and isinstance(end_clip, int):
+        pos = pos - int(end_clip)
+    return pos
+
+
+
+umi_set = set()
+with open(umi_name, "r") as fh:
     for line in fh:
         line = line.strip()
-        if line[0] == "@":
-            header.append(line)
-        else:
-            (qname,flag,rname,pos,mapq,cigar,rnext,pnext,tlen,seq,qual)=line.split()
-            
-        
-    pass
-    
+        umi_set.add(line)
+
+with open(output_name, "w") as fw:
+    with open(input_name, "r") as fr:
+        header = list()
+        # entries = list()
+        entries = dict()
+        # qname,flag,rname,pos,mapq,cigar,rnext,pnext,tlen,seq,qual = "","","","","","","","","","",""
+        for line in fr:
+            line = line.strip()
+            if line[0] == "@":
+                fw.write(f"{line}\n")
+            else:
+                # (qname,flag,chr,pos,mapq,cigar,rnext,pnext,tlen,seq,qual)=line.split("\t",11)
+                record = line.split()
+                qname = record[0]
+                flag = int(record[1])
+                chrom = record[2]
+                pos = int(record[3])
+                cigar = record[5]
+                u = re.search(r'([A-Z]+)$', qname)         # technically don't need the : in front of capturing group, but just in case
+                umi = u.group(0)
+                print(umi)
+                if umi in umi_set:
+                    fwd = check_strand(flag)
+                    start = soft_clip(cigar, fwd, pos)
+                    identifier = (umi,fwd,start)
+                    if identifier in entries.keys():
+                        entries[(umi,fwd,start)] +=1
+                    else:
+                        entries[(umi,fwd,start)] = 1
+                        fw.write(f"{line}\n")
+                        print("written")
