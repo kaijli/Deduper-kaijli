fb5dc994cc72214652a18615c0580e7c2f12bf81
kaijli <kaitlyn.j.li@gmail.com>
2022-11-01 14:24:41 -0700

    ran deduper a couple more times to iron out kinks

--- li_deduper.py
+++ li_deduper.py
@@ -9,8 +9,8 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description="A program that selects the first instance of PCR duplicates from a sorted, \
         uniquely aligned SAM file and writes a new SAM file without the PCR duplicates.")
-    parser.add_argument("-f", "--file", help="Absolute file path to deduped SAM file")
-    parser.add_argument("-o", "--outputfile", help="Absolute file path to sorted SAM file")
+    parser.add_argument("-f", "--file", help="Absolute file path to sorted SAM file for deduping")
+    parser.add_argument("-o", "--outputfile", help="Absolute file path to deduped SAM file")
     parser.add_argument("-u", "--umi", help="Name of file containing list of UMIs")
     return parser.parse_args()
 
@@ -19,61 +19,53 @@ input_name = args.file
 output_name = args.outputfile
 umi_name = args.umi
 
-# test files
-# input_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/test.sam"
-umi_name = "STL96.txt"
-# output_name = "/Users/kaitlynli/Documents/UFO/bioinformatics/Bi624/Deduper-kaijli/test_out.sam"
-# input_name = "/projects/bgmp/shared/deduper/Dataset1.sam"
-
+# umi_name = "STL96.txt"
 
 print("Files imported.")
 
-def check_strand(flag: int) -> bool:
+def cigar_parse(flag: int, cigar: str, pos: int) -> int:
     '''
-    Takes a flag integer and determines whether 16th bit is flipped.
+    Takes a flag integer from sam file and determines whether 16th bit is flipped.
     If 16th bit is flipped (1), then it's the reverse strand.
-    If 16th bit is not flipped (0), then it's the forward strand. 
-    '''
-    if (flag & 16) != 16:   # check if 16th bit is flipped
-        fwd = True          # 16th bit is not flipped, 0
-    else:
-        fwd = False         # 16th bit is flipped, 1
-    return fwd
+    If 16th bit is not flipped (0), then it's the forward strand.
 
-
-
-def cigar_parse(cigar: str, fwd: bool, pos: int) -> int:
-    '''
     Takes CIGAR string, which strand the read is on, and the left-most starting position.
     Looks for front and end softclipping in CIGAR string using 'S'.
-    Looks for deletions from reference, skipped regions fromreference, and alignment matches using D,N,M.
+    Looks for deletions from reference, skipped regions from reference, and alignment matches using D,N,M.
     Determines which clipping value to subtract from starting position based on strand.
     Using regex, 'findall' collects list of string numbers for each CIGAR variable. 
     Turn string numbers into integers using map().
     Uses sum of lists to modify 5' starting position of reads. 
     '''
-    if fwd:
+    if (flag & 16) != 16:                                           # forward strand
         front = sum(list(map(int,re.findall(r'^(\d+)S', cigar))))  
         pos = pos - front     
-    else:                                                            # reverse strand
+    else:                                                           # reverse strand
         end = sum(list(map(int,re.findall(r'(\d+)S$', cigar))))  
         deletion = sum(list(map(int,re.findall(r'(\d+)D', cigar)))) 
         skip = sum(list(map(int,re.findall(r'(\d+)N', cigar)))) 
         match = sum(list(map(int,re.findall(r'(\d+)M', cigar)))) 
         pos = pos + end + deletion + skip + match
-    # print(f"{pos=}")
     return pos
 
-
+def count_uniq_chrom(chrom: str, uniq_chroms: dict) -> dict:
+    if chrom in uniq_chroms.keys():
+        uniq_chroms[chrom] += 1
+    else:
+        uniq_chroms[chrom] = 1
+    return uniq_chroms
 
 umi_set = set()
+wrong_umi,removed = 0,0
+uniq_chroms = dict()
 with open(umi_name, "r") as fh:
     for line in fh:
         line = line.strip()
+        umi_set.add(line)
 print("UMIs extracted")
 
-with open(output_name, "w") as fw:
-    with open(input_name, "r") as fr:
+with open(output_name, "w") as fw:          #open file to write out deduped sam file
+    with open(input_name, "r") as fr:       #open file to read in sorted, aligned sam file
         entries = dict()
         for line in fr:
             line = line.strip()
@@ -82,19 +74,23 @@ with open(output_name, "w") as fw:
             else:
                 record = line.split()
                 qname = record[0]
-                flag = int(record[1])
-                chrom = record[2]
-                pos = int(record[3])
-                cigar = record[5]
-                u = re.findall(r'([A-Z]+)$', qname)
-                umi = u[0]
+                umi = re.findall(r'([A-Z]+)$', qname)[0]
                 if umi in umi_set:
-                    fwd = check_strand(flag)
-                    start = cigar_parse(cigar, fwd, pos)
-                    identifier = (umi,fwd)
+                    flag = int(record[1])
+                    chrom = record[2]
+                    pos = int(record[3])
+                    cigar = record[5]
+                    start = cigar_parse(flag, cigar, pos)
+                    identifier = (umi,chrom,start)
                     if identifier in entries.keys():
-                        entries[identifier] +=1
+                        removed += 1
                     else:
+                        uniq_chroms = count_uniq_chrom(chrom, uniq_chroms)
                         entries[identifier] = 1
                         fw.write(f"{line}\n")
+                else:
+                    wrong_umi += 1
+print(f"{wrong_umi=}")  
+print(f"{removed=}")  
+print(f"{uniq_chroms=}")                  
 print("finished")
